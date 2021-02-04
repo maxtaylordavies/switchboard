308fa1aa4c2c9fe1aafdc07a8960d984ea5220ef
Max Taylor-Davies <maxtaylordavies@gmail.com>
2021-02-04 12:04:23 +0000

    doing bits

---
+++ main.go
@@ -0,0 +1,59 @@
+package main
+
+import (
+	"net/http"
+	"net/http/httputil"
+	"net/url"
+
+	"github.com/gorilla/mux"
+	"golang.org/x/exp/errors/fmt"
+)
+
+// Server ...
+type Server struct {
+	Router      *mux.Router
+	Switchboard map[string]int
+}
+
+func (s Server) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	s.Router.ServeHTTP(w, r)
+}
+
+// ServeReverseProxy ...
+func (s *Server) ServeReverseProxy(url *url.URL, w http.ResponseWriter, r *http.Request) {
+	// create the reverse proxy
+	proxy := httputil.NewSingleHostReverseProxy(url)
+
+	r.URL.Host = url.Host
+	r.URL.Scheme = url.Scheme
+
+	r.Header.Set("X-Forwarded-Host", r.Header.Get("Host"))
+	r.Host = url.Host
+	proxy.ServeHTTP(w, r)
+}
+
+// RegisterRoutes ...
+func (s *Server) RegisterRoutes() {
+	s.Router.PathPrefix("/").HandlerFunc(
+		func(w http.ResponseWriter, r *http.Request) {
+			url := r.URL
+
+			url.Scheme = "http"
+			url.Host = fmt.Sprintf("localhost:%d", s.Switchboard[url.Host])
+
+			s.ServeReverseProxy(url, w, r)
+		},
+	).Methods("OPTIONS", "POST")
+}
+
+func main() {
+	server := Server{
+		Router: mux.NewRouter(),
+		Switchboard: map[string]int{
+			"maxtaylordavi.es": 8000,
+			"products.gallery": 8001,
+		},
+	}
+
+	server.RegisterRoutes()
+}
